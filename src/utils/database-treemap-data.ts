2fe70737abcd8e90dea8765a2ba5f579d8c6eaba
Jonathan Schaul <schauljonathan@gmail.com>
2025-08-01 08:39:28 +0200

    .

--- src/utils/database-treemap-data.ts
+++ src/utils/database-treemap-data.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import dbStats from "@/data/db-stats.json";
 
 export interface VoronoiNode {
